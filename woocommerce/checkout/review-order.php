5b371a3af9572577f270e57950e412c3c1689b5f
Peterson Macedo <petersondmb@hotmail.com>
2019-03-12 14:38:33 -0300

    alteração na descrição do produto - checkout

--- woocommerce/checkout/review-order.php
+++ woocommerce/checkout/review-order.php
@@ -38,7 +38,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 					?>
 					<tr class="<?php echo esc_attr( apply_filters( 'woocommerce_cart_item_class', 'cart_item', $cart_item, $cart_item_key ) ); ?>">
 						<td class="product-name">
-						<?php echo substr($_product->get_name(), 12); ?>
+						<?php 
+						$remove_name = strlen(get_the_title($cart_item['product_id']))+3;
+						echo substr($_product->get_name(), $remove_name); ?>
 							<!-- <?php echo apply_filters( 'woocommerce_cart_item_name', $_product->get_name(), $cart_item, $cart_item_key ) . '&nbsp;'; ?>
 							<?php echo apply_filters( 'woocommerce_checkout_cart_item_quantity', ' <strong class="product-quantity">' . sprintf( '&times; %s', $cart_item['quantity'] ) . '</strong>', $cart_item, $cart_item_key ); ?> -->
 							<?php echo wc_get_formatted_cart_item_data( $cart_item ); ?>
