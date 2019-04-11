2595be76d21942023fb786919bd6577ea36e6ae0
Peterson Macedo <petersondmb@hotmail.com>
2019-04-11 16:00:15 -0300

    adicionando paginas adw e adv1

--- header.php
+++ header.php
@@ -12,7 +12,6 @@
 <html <?php language_attributes(); ?>>
   <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
-    <meta name="robots" content="index, follow"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
