f7422bc53c40b9a16b9ef1ff0e9b832da7c30422
Jandimar <mazinhozr@gmail.com>
2019-06-19 17:14:58 -0300

    iniciando desenvolvimento do blog

---
+++ blog/header-blog.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html  lang="pt-BR">
+   <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+      <meta http-equiv="x-ua-compatible" content="ie=edge">
+      <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+      <link rel="shortcut icon " href="https://phenq-peabumxhjq.stackpathdns.com/wp-content/themes/q/icons/favicon.ico">
+
+      <link href="https://stackpath.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+      <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:100,300,400,700&display=swap" rel="stylesheet">
+      <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+      <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() ?>/blog/assets/css/main.css">
+      <script src="<?php echo get_stylesheet_directory_uri() ?>/blog/assets/js/jquery.js"></script>
+      <script src="<?php echo get_stylesheet_directory_uri() ?>/blog/assets/js/main.js"></script> 
+
+      <?php //wp_head(); ?>
+
+   </head>
+   <body <?php body_class()?>>
+      <header class="banner navbar navbar-default navbar-fixed-top" role="banner">
+         <div></div>
+         <div class="topbar">
+            <div class="container">
+               <div class="row">
+                  <div class="col-md-12">
+                     <ul class="list-inline header-options text-right">
+                        <li>
+                           <span class="click-to-call hidden-sm hidden-md hidden-lg">
+                           <a href="tel:+16465132632">
+                           <span class="glyphicon glyphicon-earphone"></span>
+                           Call us                      </a>
+                           </span>
+                           <span class="call-us hidden-xs"><span class="glyphicon glyphicon-earphone"></span> Call us : </span>
+                           <span class="phone-number hidden-xs">+1 (646) 513 2632</span>
+                        </li>
+                        <li>
+                           <a href="https://phenq.com/blog/" class="blog-link"><span class="glyphicon glyphicon-book"></span> Blog</a>
+                        </li>
+                     </ul>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="nav-bg">
+            <div class="container opacity-nav">
+               <div class="navbar-header">
+                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                  <span class="sr-only">Toggle navigation</span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  </button>
+                  <div class="pull-right hidden-sm hidden-md hidden-lg xs-order">
+                     <ul id="menu-order" class="order-nav ">
+                        <li class="btn-order menu-item menu-item-type-custom menu-item-object-custom menu-item-2572"><a title="Order Now" class="page-scroll" href="https://phenq.com/#order">Order Now</a></li>
+                     </ul>
+                  </div>
+                  <a class="navbar-brand" href="https://phenq.com/">PhenQ</a>
+               </div>
+               <nav class="collapse navbar-collapse pull-right" role="navigation">
+                  <ul id="menu-main-menu" class="nav navbar-nav">
+                     <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-2568"><a title="how it works" class="page-scroll" href="https://phenq.com/#howitworks">how it works</a></li>
+                     <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-2569"><a title="ingredients" class="page-scroll" href="https://phenq.com/#ingredients">ingredients</a></li>
+                     <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-2570"><a title="testimonials" class="page-scroll" href="https://phenq.com/#testimonials">testimonials</a></li>
+                     <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-2571"><a title="faq" class="page-scroll" href="https://phenq.com/#faq">faq</a></li>
+                  </ul>
+                  <div class="pull-right hidden-xs">
+                     <ul id="menu-order-1" class="nav order-nav ">
+                        <li class="btn-order menu-item menu-item-type-custom menu-item-object-custom menu-item-2572"><a title="Order Now" class="page-scroll" href="https://phenq.com/#order">Order Now</a></li>
+                     </ul>
+                  </div>
+               </nav>
+            </div>
+         </div>
+      </header>
